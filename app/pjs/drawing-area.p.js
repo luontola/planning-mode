01eea7af68564671f3d10ab9663ad7c056d61f2a
Esko Luontola <esko.luontola@gmail.com>
2013-11-19 18:03:23 +0200

    Removed tool.minDistance

--- app/pjs/drawing-area.p.js
+++ app/pjs/drawing-area.p.js
@@ -1,21 +1,11 @@
-// The minimum distance the mouse has to drag
-// before firing the next onMouseDrag event:
-tool.minDistance = 10;
-
 var path;
 
 function onMouseDown(event) {
-	// Create a new path and give it a stroke color:
 	path = new Path();
 	path.strokeColor = '#00000';
-
-	// Add a segment to the path where
-	// you clicked:
 	path.add(event.point);
 }
 
 function onMouseDrag(event) {
-	// Every drag event, add a segment
-	// to the path at the position of the mouse:
 	path.add(event.point);
 }
